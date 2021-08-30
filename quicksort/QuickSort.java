9fe00b74675c5a58db40af5bd611f38c41d23a9d
Lucasbertilima <57449627+Lucasbertilima@users.noreply.github.com>
2021-08-30 11:05:14 -0300

    Add files via upload

---
+++ quicksort/QuickSort.java
@@ -0,0 +1,29 @@
+public class QuickSort{
+
+    public static void main(String[] args){
+
+    }
+    public static void quicksort(int arquivo[], int posini, int posfim){
+        if (posini < posfinal){
+            
+        }
+    }
+    public static void divisor(int[] arquivo, int ini, int fim){
+        int pivo = arquivo[fim];
+        int i = ini, f = fim;
+        while(i <= f){
+            if (arquivo[i] > pivo)
+                i++;
+            else if (pivo < vetor[f])
+                f--;
+            else {
+                int troca = vetor[i];
+                vetor[i] = vetor[f];
+                vetor[f] = troca;
+                i++;
+                f--;
+         }
+        }
+    }
+    
+}
